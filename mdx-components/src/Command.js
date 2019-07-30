3e71285c0e6f2fa235cc18b13107365296a57ef8
Orestis Ioannou <o.ioannou@sysdream.com>
2019-07-30 11:30:23 +0200

    Make commands bigger

--- mdx-components/src/Command.js
+++ mdx-components/src/Command.js
@@ -1,11 +1,22 @@
 import React from "react";
 import { CodeSurferLayout, Code } from "code-surfer";
 import { Box } from "grommet";
+import styled, { createGlobalStyle } from "styled-components";
+
+const Container = styled(Box)`
+  font-size: 40px;
+`;
+const GlobalStyle = createGlobalStyle`
+  code > div:nth-child(2) {
+    overflow: visible !important;
+  }
+`;
 
 export const Command = ({ lang, code }) => (
-  <Box margin={{ top: "medium" }} height="xsmall" alignSelf="center">
+  <Container margin={{ top: "medium" }} height="small" alignSelf="center">
+    <GlobalStyle />
     <CodeSurferLayout>
       <Code lang={lang || "bash"} code={code} />
     </CodeSurferLayout>
-  </Box>
+  </Container>
 );
